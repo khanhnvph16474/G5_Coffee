d8bbd669fc945920cbdf753eca50a1b13a3a3dd0
ASUS <dongtnph24528@fpt.edu.vn>
2023-03-23 14:32:42 +0700

    code login

--- app/src/main/java/com/poly/g5_coffee/User.java
+++ app/src/main/java/com/poly/g5_coffee/User.java
@@ -1,4 +1,14 @@
 package com.poly.g5_coffee;
 
-public class User {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinhchinh);
+    }
 }
