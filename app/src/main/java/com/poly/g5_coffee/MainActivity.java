cb8668d61227b071e0fdde04e8cf0f5c20370828
Tai Chuan <chuanntph14556@fpt.edu.vn>
2023-03-24 16:17:20 +0700

    creat 5 class fragment + layout sử lý sự kiện click button navigation sửa lại màn hình splash

--- app/src/main/java/com/poly/g5_coffee/MainActivity.java
+++ app/src/main/java/com/poly/g5_coffee/MainActivity.java
@@ -1,28 +1,63 @@
 package com.poly.g5_coffee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btntieptuc);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        mNavigationView = findViewById(R.id.id_BottomNav);
+        mViewPager = findViewById(R.id.view_pager);
+        setUpViewPager();
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,login.class);
-                startActivity(intent);
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.action_Home:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_Notification:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_ShoppingCart:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                    case R.id.action_HistoryShopping:
+                        mViewPager.setCurrentItem(3);
+                        break;
+                    case R.id.action_info:
+                        mViewPager.setCurrentItem(4);
+                        break;
+                }
+
+
+                return true;
             }
         });
+    }
+
+    private void setUpViewPager(){
+        viewPagerAdapter viewPagerAdapter =
+                new viewPagerAdapter(getSupportFragmentManager(),
+                        FragmentStatePagerAdapter
+                                .BEHAVIOR_SET_USER_VISIBLE_HINT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
     }
 }
---
+++ app/src/main/java/com/poly/g5_coffee/viewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.poly.g5_coffee;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.poly.g5_coffee.Fragment.CartFragment;
+import com.poly.g5_coffee.Fragment.fragmentHistory;
+import com.poly.g5_coffee.Fragment.fragmentHome;
+import com.poly.g5_coffee.Fragment.fragmentNotification;
+import com.poly.g5_coffee.Fragment.fragmentProfile;
+
+public class viewPagerAdapter extends FragmentStatePagerAdapter {
+    public viewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new fragmentHome();
+            case 1:
+                return new fragmentNotification();
+            case 2:
+                return new CartFragment();
+            case 3:
+                return new fragmentHistory();
+            case 4:
+                return new fragmentProfile();
+            default:
+               return new fragmentHome();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
