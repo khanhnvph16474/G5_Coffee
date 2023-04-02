bc50ff88d4e55bdca59fcd26ca5fd734c685d690
Tai Chuan <chuanntph14556@fpt.edu.vn>
2023-04-02 23:53:57 +0700

    update profile

--- app/src/main/java/com/poly/g5_coffee/Activity/ChangePasswordActivity.java
+++ app/src/main/java/com/poly/g5_coffee/Activity/ChangePasswordActivity.java
@@ -2,15 +2,69 @@ package com.poly.g5_coffee.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.poly.g5_coffee.Model.User;
 import com.poly.g5_coffee.R;
+import com.poly.g5_coffee.dao.UserDao;
+
+import java.util.ArrayList;
 
 public class ChangePasswordActivity extends AppCompatActivity {
-
+    Button btn_rspass;
+    EditText edt_pass,edt_rspass;
+    UserDao dao;
+    User user;
+    ArrayList<User> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        edt_pass = findViewById(R.id.edt_pass);
+        edt_rspass = findViewById(R.id.edt_rspass);
+        btn_rspass = findViewById(R.id.btn_rsPass);
+        dao = new UserDao(ChangePasswordActivity.this);
+
+        btn_rspass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    user = new User();
+                    user.password = edt_pass.getText().toString();
+                    if( validate()>0){
+                    if(dao.update(user)>0){
+                        Toast.makeText(ChangePasswordActivity.this, "Sửa Thành Công", Toast.LENGTH_SHORT).show();
+                        edt_pass.setText("");
+                        edt_rspass.setText("");
+                    }else {
+                        Toast.makeText(ChangePasswordActivity.this, "Sửa Thất Bại", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+    }
+
+    public int validate(){
+        int check = 1;
+        if(edt_pass.getText().length()==0 || edt_rspass.getText().length()==0){
+            Toast.makeText(ChangePasswordActivity.this, "Bạn phải nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            check = -1;
+        }else {
+            String Pass = edt_pass.getText().toString();
+            String rePass = edt_rspass.getText().toString();
+            if(!Pass.equals(rePass)){
+                Toast.makeText(ChangePasswordActivity.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+        }
+        return check;
     }
 }
--- app/src/main/java/com/poly/g5_coffee/Fragment/FragmentProfile.java
+++ app/src/main/java/com/poly/g5_coffee/Fragment/FragmentProfile.java
@@ -1,7 +1,5 @@
 package com.poly.g5_coffee.Fragment;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,17 +10,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 
 import com.poly.g5_coffee.Activity.ChangePasswordActivity;
 import com.poly.g5_coffee.Activity.EditProfileUserActivity;
-import com.poly.g5_coffee.Model.Product;
 import com.poly.g5_coffee.Model.User;
 import com.poly.g5_coffee.MyDialogFragment;
 import com.poly.g5_coffee.R;
-import com.poly.g5_coffee.adapter.ProductAdapter;
 import com.poly.g5_coffee.adapter.UserAdapter;
 import com.poly.g5_coffee.dao.UserDao;
 
@@ -46,12 +41,7 @@ public class FragmentProfile extends Fragment {
         tvSdt = (TextView) view.findViewById(R.id.tv_sdt_user);
         tvdiachi = (TextView) view.findViewById(R.id.tv_Address_user);
         btn_logOut = (Button) view.findViewById(R.id.btn_logout_user);
-//        dao = new UserDao(getActivity());
-//
-//        list = (ArrayList<User>) dao.getAll();
-//        adapter = new UserAdapter(getActivity(), this, list);
 
-        //tvname.setText("" + user.name);
 
 
 
