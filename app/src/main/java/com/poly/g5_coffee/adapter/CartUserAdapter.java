ee490217a7d86141176b3c99167ed3f82518e6ca
quyen <quyenvvph20946@fpt.edu.vn>
2023-04-04 23:37:01 +0700

    abc

--- app/src/main/java/com/poly/g5_coffee/adapter/CartUserAdapter.java
+++ app/src/main/java/com/poly/g5_coffee/adapter/CartUserAdapter.java
@@ -1,4 +1,33 @@
 package com.poly.g5_coffee.adapter;
 
-public class CartUserAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CartUserAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    public class ViewHolder{
+        public TextView tv_name,tv_price,tv_soluong;
+        public Button btn_them,btn_bot;
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
